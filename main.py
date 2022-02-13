55b67c66bded3a6b5ea02d9551fd22787cfe35cf
edsonjaramillo <edsonajaramillo@gmail.com>
2022-02-13 12:22:04 -0600

    added docstrings, linting, reformat functions

--- main.py
+++ main.py
@@ -1,105 +1,123 @@
+"""This NYT Spelling Bee Bot can solve the puzzle and submit all valid words."""
+
+from time import sleep
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-from time import sleep
-from seleniumdriver import init_webdriver
+from customdriver import start_webdriver
 
 
 class NYTSpellingBeeBot:
-    driver: WebDriver = init_webdriver()
-    required_letter: str
-    other_letters: list = []
-    all_words: list = []
-    valid_words: list = []
-    invalid_letters: list = []
+    """This NYT Spelling Bee Bot can solve the puzzle and submit all valid words."""
+    _driver: WebDriver
+    _required_letter: str
+    _other_letters: list
+    _all_words: list
+    _valid_words: list
+    _invalid_letters: list
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """Initialize the webdriver and variables"""
+        self._driver = start_webdriver()
+        self._required_letter = ""
+        self._other_letters = []
+        self._all_words = []
+        self._valid_words = []
+        self._invalid_letters = []
+
+    def play_game(self) -> None:
+        """Starts the game and solves it"""
         try:
-            self.open_website()
+            self.open_url()
             self.click_play()
             self.get_letters()
             self.get_invalid_letters()
             self.get_all_words()
             self.get_valid_words()
             self.submit_words()
-        except Exception as e:
-            print("Exception below:\n", e)
         finally:
             print("Browser closed")
-            self.driver.close()
+            self._driver.close()
 
-    def open_website(self):
+    def open_url(self) -> None:
+        """Opens the url"""
         url = "https://www.nytimes.com/puzzles/spelling-bee"
-        self.driver.get(url)
+        self._driver.get(url)
         sleep(3)
 
-    def click_play(self):
-        self.driver.find_element(By.CLASS_NAME, "pz-moment__button").click()
-        sleep(1)
+    def click_play(self) -> None:
+        """Clicks the play button and waits for the puzzle to load"""
+        self._driver.find_element(By.CLASS_NAME, "pz-moment__button").click()
+        sleep(2)
 
-    # gets letters from the puzzle
-    def get_letters(self):
-        letters = self.driver.find_elements(By.CLASS_NAME, "cell-letter")
+    def get_letters(self) -> None:
+        """Gets the 7 available letters from the puzzle, the first letter is the required letter"""
+        letters = self._driver.find_elements(By.CLASS_NAME, "cell-letter")
 
-        # loop through all letters and the first one is always the required letter
         for index, letter in enumerate(letters):
             if index == 0:
-                self.required_letter = letter.text.lower()
+                self._required_letter = letter.text.lower()
             else:
-                self.other_letters.append(letter.text.lower())
+                self._other_letters.append(letter.text.lower())
 
-    # read all words from the words text file
-    def get_all_words(self):
-        with open("words.txt") as file:
-            self.all_words = file.read().splitlines()
+    def get_all_words(self) -> None:
+        """Reads all words from the words text file"""
+        with open("words.txt", "r", encoding="utf-8") as file:
+            self._all_words = file.read().splitlines()
 
-    # get all valid words with isValidWord function
-    def get_valid_words(self):
-        for word in self.all_words:
-            if self.isValidWord(word):
-                self.valid_words.append(word)
+    def get_valid_words(self) -> None:
+        """Gets all valid words from the words text file by using the is_valid_word function"""
+        for word in self._all_words:
+            if self.is_valid_word(word):
+                self._valid_words.append(word)
 
-    def isValidWord(self, word: str):
-        # if word does not have required letter return false
-        if word.find(self.required_letter) == -1:
+    def is_valid_word(self, word: str) -> bool:
+        """Checks if a word is valid according to the NYT Spelling Bee rules.
+
+        Case 1:
+            The word must contain the required letter or else it returns False.
+        Case 2:
+            Checks if the word contains any invalid letters if so it returns False.
+
+        If word passes both cases it returns True by default.
+        """
+        if word.find(self._required_letter) == -1:
             return False
 
-        # if word has invalid letters (not from the 7 possible letters) return false
         for letter in word:
-            if self.invalid_letters.count(letter) > 0:
+            if self._invalid_letters.count(letter) > 0:
                 return False
 
-        # if none of the above return true
         return True
 
-    # used to as list to check if a letter is invalid
-    def get_invalid_letters(self):
-        # start with full alphabet
+    def get_invalid_letters(self) -> None:
+        """
+        Step 1:
+            Gets invalid letters from the puzzle.
+        Step 2:
+            Starts with full alphabet & removes the required letter
+        Step 3:
+            Removes the other 6 possible letters. Doing so gives us the list of invalid letters."""
         alphabet = list("abcdefghijklmnopqrstuvwxyz")
-        # remove required letter
-        alphabet.remove(self.required_letter)
-        # remove other letters
-        for letter in self.other_letters:
+        alphabet.remove(self._required_letter)
+        for letter in self._other_letters:
             alphabet.remove(letter)
 
-        self.invalid_letters = alphabet
+        self._invalid_letters = alphabet
 
-    # submit all valid words
-    def submit_words(self):
-        # initialize action chains object
-        actions = ActionChains(self.driver)
+    def submit_words(self) -> None:
+        """Submits all valid words into the game via the selenium keyboard (ActionChains).
+        Each word from the _valid_words list is submitted & then backspaces the amount of letters.
+        This is done because the game does not give and error if you submit a word that is not
+        valid via the DOM"""
+        actions = ActionChains(self._driver)
 
-        for word in self.valid_words:
-            # type the word and press enter
+        for word in self._valid_words:
             actions.send_keys(word).send_keys(Keys.ENTER).perform()
-
-            # backspace the length of the word
-            # ex if word is "hello" backspace 5 times
-            # used in case word is not valid according to NYT
             for _ in range(len(word)):
                 actions.send_keys(Keys.BACKSPACE).perform()
 
 
 if __name__ == "__main__":
-    NYTSpellingBeeBot()
+    NYTSpellingBeeBot().play_game()
